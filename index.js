6f4e9aa013a72ac2ea252c7a080fac56bc07d3c3
Anmol Agrawal <anmol1771@gmail.com>
2016-01-05 22:48:11 +0530

    added some comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var r = require('rethinkdb');
+var r = require('rethinkdb'); // Initialise rethinkdb instance
 
-var pubnub = require("pubnub")({
+var pubnub = require("pubnub")({ // Initialise pubnub instance
     subscribe_key: 'demo', // always required
     publish_key: 'demo'    // only required if publishing
 });
@@ -11,17 +11,18 @@ r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) throw err;
     connection = conn;
 
-// get a book
-    r.table('books').filter(r.row('name').eq("Harry Potter Series"))
-      .changes()
-      .run(connection, function(err, cursor) {
+// Get a book
+    r.table('books').filter(r.row('name').eq("Harry Potter Series")) // Get a  book with name 'Harry Potter'
+      .changes() // Look for any changes happening to this record and keeps returning it
+      .run(connection, function(err, cursor) { // run the above query on this connection
         if (err) throw err;
-        cursor.each(function(err, result) {
+        cursor.each(function(err, result) { // If there is no error, it returns a cursor, which is collection of JSON objects
             if (err) throw err;
 
-            pubnub.publish({  //publishing the updated seat numbers through PubNub, in 'rethinkdb-pubnub' channel
+            pubnub.publish({  //publishing the updated Book's status through PubNub, in 'rethinkdb-pubnub' channel
               channel: "rethinkdb-pubnub",
               message: {"status" : result.new_val.availability_status}, //this is the message payload we are sending
+              // result contains a new value and the old value of this record. This is due to .changes() method.
               callback: function(m){console.log(m)}
             });
             console.log({"status" : result.new_val.availability_status});
